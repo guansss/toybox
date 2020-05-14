09c9ad54dad6be31345392b4dc551bd13bdf4f51
guansss <821143943@qq.com>
2020-05-14 20:25:29 +0800

    Update information in register_bones

--- src/mmd/register_bones.py
+++ src/mmd/register_bones.py
@@ -1,5 +1,15 @@
 """
 Batch registers/unregisters OP bones in MMD.
+
+Usage:
+
+Set FRAME_FIRST and FRAME_FINAL respectively to the start and end of frames that you want to edit.
+When FRAME_FIRST is greater than FRAME_FINAL, frame operations will be done from right to left.
+
+You would also need to modify those mouse-clicking coordinates in configs, there's a script at
+"misc/display_cursor_pos.py" to help with this.
+
+Then run the script and remember to switch to the MMD window.
 """
 
 import threading
@@ -16,7 +26,7 @@ FRAME_FINAL = 1
 
 OP_BUTTON = (337, 1357)
 
-TARGET_BONE_DROPDOWN = (1204, 675)
+TARGET_BONE_DROPDOWN = (1204, 635)
 TARGET_BONE = (1183, 661)
 
 OP_MODEL_DROPDOWN = (1209, 676)
@@ -51,11 +61,13 @@ def run(panel):
 
     frame_range = range(min(FRAME_FIRST, FRAME_FINAL), max(FRAME_FIRST, FRAME_FINAL) + 1)
 
+    total = len(frame_range)
+
     if SHIFT_KEY == 'left':
         frame_range = reversed(frame_range)
 
-    for frame in frame_range:
-        panel.display('Frame: %s' % frame)
+    for i, frame in enumerate(frame_range):
+        panel.display('Frame: %s (%s/%s)' % (frame, i + 1, total))
 
         register_frame()
 
